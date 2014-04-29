5810ac561518d8a5f3fae243638dd4baded2d05e
jdcalvin <calvjd@gmail.com>
2014-04-29 09:31:51 -0700

    first commit

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source "https://rubygems.org"
+ruby "2.1.1"
+
+gem "foreman"
+gem "pony"
+gem "sinatra"
+gem "unicorn"
---
+++ app.rb
@@ -0,0 +1,33 @@
+require "rubygems"
+require "sinatra"
+require "pony"
+
+require_relative "models/delivery_config"
+
+# Configuration
+DELIVERY_CONFIG = DeliveryConfig.new(ENV)
+Pony.options = DELIVERY_CONFIG.pony_options
+
+class App < Sinatra::Application
+  get "/" do
+    "Contact form is up\n"
+  end
+
+  post "/" do
+    form_contents = params.map do |key, value|
+      "#{key}: #{value}"
+    end.join("\n")
+    puts "Sending:", form_contents
+    puts "Referer: #{request.referer}"
+    puts "Pony config: #{Pony.options.inspect}"
+
+    Pony.mail(
+      to: DELIVERY_CONFIG.recipient,
+      from: DELIVERY_CONFIG.sender,
+      subject: "Contact Form Submission",
+      body: form_contents,
+    )
+
+    redirect DELIVERY_CONFIG.redirect || request.referrer
+  end
+end
